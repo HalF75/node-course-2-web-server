44c884fbdc611a156722322e0c0300a145f70d6e
Ricardo Carvalho <ricardo.carvalho@mac-c02sd265g8wm.bouvet.no>
2018-08-09 12:11:44 +0200

    New project page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project page',
+        projectText: 'My Project list'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error handling the request'
